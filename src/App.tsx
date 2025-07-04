1f0ebf0688d4af49e05d957303d81341d5f7cbbc
phoreverr <forever@codematic.io>
2025-07-04 13:36:38 +0100

    feat: teatarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[500px] h-[300px] border-2 border-blue-500 p-4 rounded-lg shadow-lg mx-auto mt-10">
+      <div className='flex justify-center'>
+
+      </div>
+      <textarea
+        className="w-full h-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-0 focus:border-gray-300 resize-none"
+      ></textarea>
     </div>
   );
 }
